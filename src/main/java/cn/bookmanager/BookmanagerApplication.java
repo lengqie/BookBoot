dc49c6552adf0f84db5f3f0a8c41935a7f68c325
lengqie <lengqie@foxmail.com>
2021-04-19 09:44:56 +0800

    default

--- src/main/java/cn/bookmanager/BookmanagerApplication.java
+++ src/main/java/cn/bookmanager/BookmanagerApplication.java
@@ -1,5 +1,8 @@
 package cn.bookmanager;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +12,21 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @EnableOpenApi
 public class BookmanagerApplication {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         SpringApplication.run(BookmanagerApplication.class, args);
+
+        // QuickStart();
+
+    }
+    public static void  QuickStart(){
+        Runtime run = Runtime.getRuntime();
+        try {
+            Process process = run.exec("cmd /k start http://localhost:8081");
+            System.out.println("http://localhost:8081");
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
     }
 
 }
